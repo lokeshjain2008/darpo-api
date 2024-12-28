6907b601d65bea658c895d72cef9c28c22bb2753
Lokesh <ljain@truedata.co>
2024-12-28 22:20:02 +0530

    fix: auth module review app start

--- src/common/guards/tenant.guard.ts
+++ src/common/guards/tenant.guard.ts
@@ -1,27 +1,29 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { PrismaService } from '../../prisma/prisma.service';
+// import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+// import { PrismaService } from '../../prisma/prisma.service';
 
-@Injectable()
-export class TenantGuard implements CanActivate {
-  constructor(private prisma: PrismaService) {}
+// // don't add this guard to the providers array in the module
+// // Not in use in this project, but it's a good example of how to use guards
+// // @Injectable()
+// export class TenantGuard implements CanActivate {
+//   constructor(private prisma: PrismaService) {}
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const tenantId = request.headers['x-tenant-id'];
+//   async canActivate(context: ExecutionContext): Promise<boolean> {
+//     const request = context.switchToHttp().getRequest();
+//     const tenantId = request.headers['x-tenant-id'];
 
-    if (!tenantId) {
-      return false;
-    }
+//     if (!tenantId) {
+//       return false;
+//     }
 
-    const tenant = await this.prisma.tenant.findUnique({
-      where: { id: tenantId },
-    });
+//     const tenant = await this.prisma.tenant.findUnique({
+//       where: { id: tenantId },
+//     });
 
-    if (!tenant) {
-      return false;
-    }
+//     if (!tenant) {
+//       return false;
+//     }
 
-    request.tenant = tenant;
-    return true;
-  }
-}
+//     request.tenant = tenant;
+//     return true;
+//   }
+// }
