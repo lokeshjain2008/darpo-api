68b542f99152ebc729c86e6deeca34da8669eb7e
Lokesh <ljain@truedata.co>
2024-12-31 16:40:11 +0530

    feat: authGuard changes

--- src/modules/auth/guards/public.guard.ts
+++ src/modules/auth/guards/public.guard.ts
@@ -6,7 +6,10 @@ export class PublicGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const isPublic = this.reflector.get<boolean>('isPublic', context.getHandler());
+    const isPublic = this.reflector.get<boolean>(
+      'isPublic',
+      context.getHandler(),
+    );
     return isPublic;
   }
 }
