36a0fec322758f5228585bc556a190ecf1bd9a62
Lokesh <ljain@truedata.co>
2025-01-04 12:50:18 +0530

    feat: wworking on git action

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './modules/auth/auth.module';
-import { VisitorModule } from './modules/visitor/visitor.module';
-import { UserModule } from './modules/user/user.module';
-import { AdminModule } from './modules/admin/admin.module';
+// import { AuthModule } from './modules/auth/auth.module';
+// import { VisitorModule } from './modules/visitor/visitor.module';
+// import { UserModule } from './modules/user/user.module';
+// import { AdminModule } from './modules/admin/admin.module';
 
 @Module({
-  imports: [AuthModule, VisitorModule, UserModule, AdminModule],
+  // imports: [AuthModule, VisitorModule, UserModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
